cc0b99e75ae552332f72f361e650f57292361c16
estercorreabarcelos-coder <estercorreabarcelos@gmail.com>
2026-09-17 16:10:27 -0300

    LUZ - to no computador da ester porque o castillo nao me colocou no doc git
    giovanna luz mexendo :)

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System.Xml;
+
+
+Cinema meuCinema = new Cinema();
+
+if(meuCinema.Poltronas[1,c])
+{
+    Console.WriteLine(" [ XX ] ");
+}
+
+if(meuCinema.ReservarAssento(indiceLinha,indiceColuna))
+{
+    Console.WriteLine("Assento reservado com sucesso!");
+}
+else
+{
+    Console.WriteLine("Este assento ja esta ocupado!");
+}
