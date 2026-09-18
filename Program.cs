2a165a8fea32174eca05d7bfdd56e5ca86a4284b
Luzzzkkkkk <Luznerdola@gmail.com>
2026-09-18 08:49:17 -0300

    oi castillo me colocou no doc git

--- Program.cs
+++ Program.cs
@@ -5,43 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("=== INICIANDO TESTES DO CRUD COMPLETO ===\n");
-
-        // ==========================================
-        // 1. CREATE (CADASTRAR)
-        // ==========================================
+////CRIAR////
         Console.WriteLine("--- 1. Testando Cadastro (CREATE) ---");
         
         string filmeCadastro = "Vingadores: Ultimato";
         double precoCadastro = 45.50;
 
         Cinema novoCinema = new Cinema(precoCadastro, filmeCadastro);
-        
-        // Reservando assentos no código
         novoCinema.ReservarAssento(1, 1);
         novoCinema.ReservarAssento(1, 2);
 
         novoCinema.Cadastrar();
-
-
-        // ==========================================
-        // 2. READ (LISTAR TODOS)
-        // ==========================================
+///LISTAGEM//
         Console.WriteLine("\n--- 2. Testando Listagem Geral (READ) ---");
         Cinema.Listar();
-
-
-        // ==========================================
-        // 3. READ (BUSCAR POR ID)
-        // ==========================================
+////BUSCAR////
         Console.WriteLine("\n--- 3. Testando Busca por ID (READ) ---");
         
         int idParaBuscar = 1; 
         Cinema.Buscar(idParaBuscar);
-
-
-        // ==========================================
-        // 4. UPDATE (ATUALIZAR)
-        // ==========================================
+////ATUALIZAR////
         Console.WriteLine("\n--- 4. Testando Atualização (UPDATE) ---");
         
         int idParaEditar = 1;
@@ -53,20 +36,12 @@ class Program
         cinemaEditado.QuantidadePoltronasOcupadas = 4;
 
         cinemaEditado.Atualizar();
-
-
-        // ==========================================
-        // 5. DELETE (EXCLUIR)
-        // ==========================================
+////DELETAR////
         Console.WriteLine("\n--- 5. Testando Exclusão (DELETE) ---");
         
         int idParaDeletar = 2; 
         Cinema.Excluir(idParaDeletar);
 
-
-        // ==========================================
-        // FINALIZAÇÃO
-        // ==========================================
         Console.WriteLine("\n=========================================");
         Console.WriteLine("===    TODOS OS TESTES FINALIZADOS    ===");
         Console.WriteLine("=========================================");
