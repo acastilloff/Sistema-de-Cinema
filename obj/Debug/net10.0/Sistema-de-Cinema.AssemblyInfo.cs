7b2761f59f20e4fd4f7357495992f32ffe001b93
estercorreabarcelos-coder <estercorreabarcelos@gmail.com>
2026-09-21 14:04:18 -0300

    sla, ta dando 44  vou me tacar da janela

--- obj/Debug/net10.0/Sistema-de-Cinema.AssemblyInfo.cs
+++ obj/Debug/net10.0/Sistema-de-Cinema.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema-de-Cinema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af1c0c10d7cb60b21b56aa74e66e38263f9b5ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182982cc5fd43ba12acfd3aaf30bc95022d831d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema-de-Cinema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema-de-Cinema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
